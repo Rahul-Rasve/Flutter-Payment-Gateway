0de83fe3a574efecbed5c7c2c47c22f3ff757cf0
Rahul Rasve <rahulrasve712@gmail.com>
2024-10-19 13:15:29 +0530

    auth screens dome

--- client/lib/screens/signup.dart
+++ client/lib/screens/signup.dart
@@ -1,5 +1,4 @@
 import 'package:client/bloc/signup_bloc';
-import 'package:client/screens/home.dart';
 import 'package:client/screens/login.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
