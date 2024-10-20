cfa8a862815543f1a6e37350099a33a988c62c67
Rahul Rasve <rahulrasve712@gmail.com>
2024-10-20 10:23:39 +0530

    verify payment done

--- server/controllers/PaymentController.js
+++ server/controllers/PaymentController.js
@@ -2,39 +2,79 @@ const Razorpay = require("razorpay");
 const Payment = require("../models/paymentSchema");
 
 const razorpay = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_SECRET_KEY,
+	key_id: process.env.RAZORPAY_KEY_ID,
+	key_secret: process.env.RAZORPAY_SECRET_KEY,
 });
 
 const createPaymentOrder = async (req, res) => {
-    try {
-        const options = {
-            amount: req.body.amount * 100,
-            currency: "INR",
-            receipt: "receipt_" + Date.now(),
-        }
+	try {
+		const options = {
+			amount: req.body.amount * 100,
+			currency: "INR",
+			receipt: "receipt_" + Date.now(),
+		};
 
-        const order = await razorpay.orders.create(options);
+		const order = await razorpay.orders.create(options);
 
-        const newPayment = await Payment({
-            paymentId: order.id,
-            amount: order.amount,
-            currency: order.currency,
-            status: order.status,
-            userId: req.body.userId,
-        }).save();
+		const newPayment = await Payment({
+			paymentId: order.id,
+			amount: order.amount,
+			currency: order.currency,
+			status: order.status,
+			userId: req.body.userId,
+		}).save();
 
-        return res.status(201).send({
-            success: true,
-            message: "Payment created successfully",
-            data: newPayment,
-        });
+		return res.status(201).send({
+			success: true,
+			message: "Payment created successfully",
+			data: newPayment,
+		});
+	} catch (error) {
+		return res.status(500).send({
+			success: false,
+			message: "Error creating payment order",
+			data: error,
+		});
+	}
+};
 
-    } catch (error) {
-        
-    }
-}
+const verifyPayment = async (req, res) => {
+	try {
+		const { razorpay_signature, razorpay_payment_id, razorpay_order_id } =
+			req.body;
+
+		//verify signature
+		const generated_signature = crypto
+			.createHmac("sha256", process.env.RAZORPAY_SECRET_KEY)
+			.update(razorpay_order_id + "|" + razorpay_payment_id)
+			.digest("hex");
+
+		if (generated_signature !== razorpay_signature) {
+			return res.status(400).send({
+				success: false,
+				message: "Invalid signature sent!",
+			});
+		}
+
+		await Payment.findOneAndUpdate(
+			{ paymentId: razorpay_payment_id },
+			{ status: "PAID" }
+		);
+
+		return res.status(200).send({
+			success: true,
+			message: "Payment verified successfully",
+		});
+	} catch (error) {
+		return res.status(500).send({
+			success: false,
+			message: "Error verifying payment order",
+			data: error,
+		});
+	}
+};
 
 module.exports = {
-    createPaymentOrder,
+	createPaymentOrder,
+	verifyPayment,
 };
--- server/routes/PaymentRoutes.js
+++ server/routes/PaymentRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { createPaymentOrder } = require("../controllers/PaymentController");
+const {
+	createPaymentOrder,
+	verifyPayment,
+} = require("../controllers/PaymentController");
 
 const router = express.Router();
 
 router.post("/order", createPaymentOrder);
+router.post("/verify-payment", verifyPayment);
 
 module.exports = router;
