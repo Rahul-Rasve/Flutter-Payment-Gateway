027c44e87c4bdca4b2708aa65696c75b074d8379
Rahul Rasve <rahulrasve712@gmail.com>
2024-10-21 12:57:26 +0530

    added api new services

--- client/lib/api/api_client.dart
+++ client/lib/api/api_client.dart
@@ -1,5 +1,6 @@
 import 'package:client/api/interceptor.dart';
 import 'package:client/models/api_response.dart';
+import 'package:client/models/user_portfolio_model.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
@@ -39,7 +40,10 @@ class ApiClient {
   }
 
   Future<ApiResponse> signUpUser(
-      String? name, String? email, String? pasword) async {
+    String? name,
+    String? email,
+    String? pasword,
+  ) async {
     Map<String, dynamic> payload = {
       "name": name,
       "email": email,
@@ -102,6 +106,184 @@ class ApiClient {
       } else {
         apiResponse.resultStatus = ResultStatus.failure;
         apiResponse.message = reponse.data["message"];
+      }
+      return apiResponse;
+    } catch (e) {
+      debugPrint(e.toString());
+      apiResponse.resultStatus = ResultStatus.failure;
+      apiResponse.message = "Error fetching response";
+      return apiResponse;
+    }
+  }
+
+  Future<ApiResponse> getPortfolio(String userId) async {
+    final ApiResponse apiResponse = ApiResponse();
+    try {
+      final Response response = await dio.get(
+        "/payment/portfolio-stats",
+        data: {userId: userId},
+      );
+      if (response.statusCode == 200) {
+        apiResponse.resultStatus = ResultStatus.success;
+        apiResponse.message = response.data["message"];
+        apiResponse.responseData =
+            UserPortfolio.fromJson(response.data["data"]);
+      } else {
+        apiResponse.resultStatus = ResultStatus.failure;
+        apiResponse.message = response.data["message"];
+      }
+      return apiResponse;
+    } catch (e) {
+      debugPrint(e.toString());
+      apiResponse.resultStatus = ResultStatus.failure;
+      apiResponse.message = "Error fetching response";
+      return apiResponse;
+    }
+  }
+
+  Future<ApiResponse> getTransactionHistory(String userId) async {
+    final ApiResponse apiResponse = ApiResponse();
+    try {
+      final Response response = await dio.get(
+        "/payment/transaction-history",
+        data: {userId: userId},
+      );
+      if (response.statusCode == 200) {
+        apiResponse.resultStatus = ResultStatus.success;
+        apiResponse.message = response.data["message"];
+        apiResponse.responseData = response.data["data"];
+      } else {
+        apiResponse.resultStatus = ResultStatus.failure;
+        apiResponse.message = response.data["message"];
+      }
+      return apiResponse;
+    } catch (e) {
+      debugPrint(e.toString());
+      apiResponse.resultStatus = ResultStatus.failure;
+      apiResponse.message = "Error fetching response";
+      return apiResponse;
+    }
+  }
+
+  Future<ApiResponse> handleDeposits(double amount, String userId) async {
+    final ApiResponse apiResponse = ApiResponse();
+    try {
+      final Response response = await dio.post(
+        "/payment/deposit",
+        data: {
+          "amount": amount,
+          "userId": userId,
+        },
+      );
+      if (response.statusCode == 200) {
+        apiResponse.resultStatus = ResultStatus.success;
+        apiResponse.message = response.data["message"];
+        apiResponse.responseData = response.data["data"];
+      } else {
+        apiResponse.resultStatus = ResultStatus.failure;
+        apiResponse.message = response.data["message"];
+      }
+      return apiResponse;
+    } catch (e) {
+      debugPrint(e.toString());
+      apiResponse.resultStatus = ResultStatus.failure;
+      apiResponse.message = "Error fetching response";
+      return apiResponse;
+    }
+  }
+
+  Future<ApiResponse> handleWithdraw(double amount, String userId) async {
+    final ApiResponse apiResponse = ApiResponse();
+    try {
+      final Response response = await dio.post(
+        "/payment/withdraw",
+        data: {
+          "amount": amount,
+          "userId": userId,
+        },
+      );
+      if (response.statusCode == 200) {
+        apiResponse.resultStatus = ResultStatus.success;
+        apiResponse.message = response.data["message"];
+        apiResponse.responseData = response.data["data"];
+      } else {
+        apiResponse.resultStatus = ResultStatus.failure;
+        apiResponse.message = response.data["message"];
+      }
+      return apiResponse;
+    } catch (e) {
+      debugPrint(e.toString());
+      apiResponse.resultStatus = ResultStatus.failure;
+      apiResponse.message = "Error fetching response";
+      return apiResponse;
+    }
+  }
+
+  Future<ApiResponse> getGoldPrice() async {
+    final ApiResponse apiResponse = ApiResponse();
+    try {
+      final Response response = await dio.get("/payment/gold-price");
+      if (response.statusCode == 200) {
+        apiResponse.resultStatus = ResultStatus.success;
+        apiResponse.message = response.data["message"];
+        apiResponse.responseData = response.data["goldPrice"];
+      } else {
+        apiResponse.resultStatus = ResultStatus.failure;
+        apiResponse.message = response.data["message"];
+      }
+      return apiResponse;
+    } catch (e) {
+      debugPrint(e.toString());
+      apiResponse.resultStatus = ResultStatus.failure;
+      apiResponse.message = "Error fetching response";
+      return apiResponse;
+    }
+  }
+
+  Future<ApiResponse> handleBuyGold(double amount, String userId) async {
+    final ApiResponse apiResponse = ApiResponse();
+    try {
+      final response = await dio.post(
+        "/payment/buy-gold",
+        data: {
+          "amount": amount,
+          "userId": userId,
+        },
+      );
+      if (response.statusCode == 200) {
+        apiResponse.resultStatus = ResultStatus.success;
+        apiResponse.message = response.data["message"];
+        apiResponse.responseData = response.data["data"];
+      } else {
+        apiResponse.resultStatus = ResultStatus.failure;
+        apiResponse.message = response.data["message"];
+      }
+      return apiResponse;
+    } catch (e) {
+      debugPrint(e.toString());
+      apiResponse.resultStatus = ResultStatus.failure;
+      apiResponse.message = "Error fetching response";
+      return apiResponse;
+    }
+  }
+
+  Future<ApiResponse> handleSellGold(double amount, String userId) async {
+    final ApiResponse apiResponse = ApiResponse();
+    try {
+      final response = await dio.post(
+        "/payment/sell-gold",
+        data: {
+          "amount": amount,
+          "userId": userId,
+        },
+      );
+      if (response.statusCode == 200) {
+        apiResponse.resultStatus = ResultStatus.success;
+        apiResponse.message = response.data["message"];
+        apiResponse.responseData = response.data["data"];
+      } else {
+        apiResponse.resultStatus = ResultStatus.failure;
+        apiResponse.message = response.data["message"];
       }
       return apiResponse;
     } catch (e) {
--- server/routes/PaymentRoutes.js
+++ server/routes/PaymentRoutes.js
@@ -15,9 +15,9 @@ const router = express.Router();
 router.post("/order", createPaymentOrder);
 router.post("/verify-payment", verifyPayment);
 router.post("/deposit", handleDeposits);
-router.get("/gold-price", getGoldPrice);
 router.post("/buy-gold", handleBuyGold);
 router.post("/sell-gold", handleSellGold);
+router.get("/gold-price", getGoldPrice);
 router.get("/transaction-history", getTransactionHistory);
 router.get("/portfolio-stats", getPortfolioStats);
 
